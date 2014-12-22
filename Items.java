7e408677fc99dc0562b24f8c28720af9338579bd
Jason Shin <jasonshin98@gmail.com>
2014-12-22 08:28:15 -0500

    Added Items and VM

---
+++ Items.java
@@ -0,0 +1,17 @@
+public class Items {
+
+    private String name;
+    private double Bprice;
+    private double Sprice;
+    private int stocks;
+        
+    public Items (String n, double b, double s) {
+	name = n;
+	Bprice = b;
+	Sprice = s;
+    }
+
+    
+    
+    
+}
---
+++ VM.java
@@ -0,0 +1,7 @@
+public class VM {
+
+    public static void main(String[] args) {
+	ArrayList<String> itemNames = new ArrayList<String>();
+	itemNames.add(
+	ArrayList<Items> items = new ArrayList<Items>();
+	for int i < 30
