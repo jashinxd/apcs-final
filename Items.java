ce7b22198704b8cd3f1e9a8e5b0cf9f1e9f2c370
Shi Shu <shishu_1998@yahoo.com>
2014-12-22 08:36:40 -0500

    Items2 added

--- Items.java
+++ Items.java
@@ -1,14 +1,48 @@
-public class Items {
+public class Items2 {
 
     private String name;
     private double Bprice;
     private double Sprice;
     private int stocks;
         
-    public Items (String n, double b, double s) {
+    public Items2 (String n) {
 	name = n;
-	Bprice = b;
-	Sprice = s;
+	if (name.equals("Beef Jerky")){
+	    Bprice = 1.0;
+	    Sprice = 1.5;
+	}
+	if (name.equals("Sliced Apples")){
+	    Bprice = 0.75;
+	    Sprice = 1.0;
+	}
+	if (name.equals("Potato Chips")){
+	    Bprice = 0.5;
+	    Sprice = 1.0;
+	}
+	if (name.equals("Chocolate Chip Cookies")){
+	    Bprice = 1.0;
+	    Sprice = 1.75;
+	}
+	if (name.equals("Oatmeal Cookies")){
+	    Bprice = 1.0;
+	    Sprice = 1.5;
+	}
+	if (name.equals("Airheads")){
+	    Bprice = 0.5;
+	    Sprice = 1.0;
+	}
+	if (name.equals("Nacho Chips")){
+	    Bprice = 0.5;
+	    Sprice = 1.0;
+	}
+	if (name.equals("Granola Bar")){
+	    Bprice = 0.25;
+	    Sprice = 1.0;
+	}
+	if (name.equals("SunChips")){
+	    Bprice = 0.25;
+	    Sprice = 1.0;
+	}
     }
 
     
