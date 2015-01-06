74a9f1e1294bae67b3459c6c1b9a43ec5b89f0b1
Shi Shu <shishu_1998@yahoo.com>
2015-01-06 08:40:39 -0500

    Machine

--- Machine.java
+++ Machine.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Machine extends JFrame{
+    private JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20;
     public Machine(){
 	setTitle("Vending Machine");
 	setSize(600,600);
