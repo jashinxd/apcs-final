8d03ebc453f07ed6380c039f987a1baee900fdaf
shishu1998 <shishu_1998@yahoo.com>
2015-01-05 19:08:39 -0500

    Update

--- Items.java
+++ Items.java
@@ -1,51 +1,70 @@
+import java.io.*;
+import java.util.*;
 public class Items{
-
     private String name;
     private double Bprice;
     private double Sprice;
     private int stocks;
-        
-    public Items(String n) {
+    public Items(String n){
+	Random r = new Random();
+	int s = r.nextInt(15) + 1;
 	name = n;
-	if (name.equals("Beef Jerky")){
+	if (n.equals("Beef Jerky")){
 	    Bprice = 1.0;
 	    Sprice = 1.5;
+	    stocks = s;
 	}
-	if (name.equals("Sliced Apples")){
+	if (n.equals("Sliced Apples")){
 	    Bprice = 0.75;
 	    Sprice = 1.0;
+	    stocks = s;
 	}
-	if (name.equals("Potato Chips")){
+	if (n.equals("Potato Chips")){
 	    Bprice = 0.5;
 	    Sprice = 1.0;
+	    stocks = s;
 	}
-	if (name.equals("Chocolate Chip Cookies")){
+	if (n.equals("Chocolate Chip Cookies")){
 	    Bprice = 1.0;
 	    Sprice = 1.75;
+	    stocks = s;
 	}
-	if (name.equals("Oatmeal Cookies")){
+	if (n.equals("Oatmeal Cookies")){
 	    Bprice = 1.0;
 	    Sprice = 1.5;
+	    stocks = s;
 	}
-	if (name.equals("Airheads")){
+	if (n.equals("Airheads")){
 	    Bprice = 0.5;
 	    Sprice = 1.0;
+	    stocks = s;
 	}
-	if (name.equals("Nacho Chips")){
+	if (n.equals("Nacho Chips")){
 	    Bprice = 0.5;
 	    Sprice = 1.0;
+	    stocks = s;
 	}
-	if (name.equals("Granola Bar")){
+	if (n.equals("Granola Bar")){
 	    Bprice = 0.25;
 	    Sprice = 1.0;
+	    stocks = s;
 	}
-	if (name.equals("SunChips")){
+	if (n.equals("SunChips")){
 	    Bprice = 0.25;
 	    Sprice = 1.0;
+	    stocks = s;
 	}
     }
-
-    
-    
-    
+    public String getname(){
+	return name;
+    }
+    public double getbprice(){
+	return Bprice;
+    }
+    public double getsprice(){
+	return Sprice;
+    }
+    public int getstocks(){
+	return stocks;
+    }
 }
