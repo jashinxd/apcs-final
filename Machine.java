2ab369f848e1d58ea4a8a7be621c0f556eac924b
Jason Shin <jasonshin98@gmail.com>
2015-01-09 08:40:41 -0500

    Added Text Area

--- Machine.java
+++ Machine.java
@@ -4,24 +4,38 @@ import java.awt.event.*;
 
 public class Machine extends JFrame{
     private Container pane;
-    private JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20;
     private JButton[] Buttons = new JButton[20];
-    JButton[] Button = {b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20};
+    private JTextArea text;
+    private JPanel machine;
     private VM jason;
+    
     public Machine(){
 	setTitle("Vending Machine");
 	setSize(600,600);
 	setLocation(500,200);
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	
+	setDefaultCloseOperation(EXIT_ON_CLOSE);	
+
 	pane = getContentPane();
-	pane.setLayout(new GridLayout(5,4,25,25));
+	pane.setLayout(new FlowLayout());
+
+	text = new JTextArea();
+	text.setColumns(40);
+	text.setRows(10);
+	text.setBorder(BorderFactory.createLineBorder(Color.red,2));
+	pane.add(text);
+	
+	JPanel machine = new JPanel();
+	machine.setLayout(new GridLayout(4,5));
 	VM jason = new VM();
 	for(int i = 0;i<20;i ++){
 	    Buttons[i] = new JButton(jason.getItems().get(i).getname());
-	    pane.add(Buttons[i]);
+	    machine.add(Buttons[i]);
 	}
-     
+	setPreferredSize(new Dimension(400,400));
+	machine.setBorder(BorderFactory.createLineBorder(Color.blue,2));
+	pane.add(machine);
+	
+	
     }
     public static void main(String[] args){
 	Machine a = new Machine();
