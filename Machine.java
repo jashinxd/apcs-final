6d8785237aeea6f1f7170805e9b5af4af772c8d3
Jason Shin <jasonshin98@gmail.com>
2015-01-10 15:11:48 -0500

    Added ActionListener

--- Machine.java
+++ Machine.java
@@ -2,11 +2,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Machine extends JFrame{
+public class Machine extends JFrame implements ActionListener{
     private Container pane;
     private JButton[] Buttons = new JButton[20];
+    private double[] sprices = new double[20];
     private JTextArea text;
     private JPanel machine;
+    private JLabel price;
     private VM jason;
     
     public Machine(){
@@ -18,25 +20,44 @@ public class Machine extends JFrame{
 	pane = getContentPane();
 	pane.setLayout(new FlowLayout());
 
+	price = new JLabel("Price:");
+	pane.add(price);
+
 	text = new JTextArea();
-	text.setColumns(40);
-	text.setRows(10);
+	text.setColumns(10);
+	text.setRows(1);
+	text.setEditable(false);
+	//text.setBackground(Color.BLACK);
 	text.setBorder(BorderFactory.createLineBorder(Color.red,2));
 	pane.add(text);
 	
 	JPanel machine = new JPanel();
-	machine.setLayout(new GridLayout(4,5));
+	machine.setLayout(new GridLayout(10,2));
 	VM jason = new VM();
 	for(int i = 0;i<20;i ++){
 	    Buttons[i] = new JButton(jason.getItems().get(i).getname());
+	    sprices[i] = jason.getItems().get(i).getsprice();
+	    Buttons[i].setPreferredSize(new Dimension(250,30));
 	    machine.add(Buttons[i]);
+	    Buttons[i].addActionListener(this);
 	}
-	setPreferredSize(new Dimension(400,400));
+	//machine.setSize(600,600);
 	machine.setBorder(BorderFactory.createLineBorder(Color.blue,2));
 	pane.add(machine);
-	
-	
+        for (int i = 0; i < 20; i++) {
+	    System.out.println(jason.getItems().get(i).getsprice());
+	}
     }
+
+    public void actionPerformed(ActionEvent e) {
+	for (int i = 0; i < 20; i++) {	    
+	    if (e.getSource() == Buttons[i]) {
+		text.setText(""+sprices[i]);
+		//text.setText("hello");
+	    }
+	}
+    }
+
     public static void main(String[] args){
 	Machine a = new Machine();
 	a.setVisible(true);
